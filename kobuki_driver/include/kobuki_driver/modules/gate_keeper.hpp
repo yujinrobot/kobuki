dfe19aaa4718a57fe6a2f86336c46c74095df2a1
Younghun Ju <yhju83@gmail.com>
2012-06-01 18:30:01 +0900

    kobuki_drive : Updated 'high acceleration smoothing' feature, but there are still uncomfortable things.

--- kobuki_driver/include/kobuki_driver/modules/gate_keeper.hpp
+++ kobuki_driver/include/kobuki_driver/modules/gate_keeper.hpp
@@ -44,6 +44,7 @@
 *****************************************************************************/
 
 #include <stdint.h>
+#include <ecl/time.hpp>
 
 /*****************************************************************************
 ** Namespaces
@@ -60,7 +61,7 @@ public:
   GateKeeper() :
     is_enabled(true),
     last_speed(0), 
-    last_timestamp(0)
+    last_timestamp(ecl::TimeStamp())
   {}
   void init(bool enable_gate_keeper){ is_enabled = enable_gate_keeper; }
   void confirm(short &speed) // or smoother, limiter, etc
@@ -68,17 +69,42 @@ public:
     if( is_enabled )
     {
       //get current time
+      ecl::TimeStamp curr_timestamp;
       //get time difference
+      ecl::TimeStamp duration = curr_timestamp - last_timestamp;
       //calculate acceleration
+      double acceleration = ((double)(speed - last_speed)) / duration; // in mm/s^2
+
       //if criterion meet some condition, limit input velocity in certain step
-      ;
+      std::ostringstream oss;
+      oss << "[" << duration << "]";
+      oss << "[" << last_speed << "]";
+      oss << "[" << speed << "]";
+      oss << "[" << acceleration << "]";
+
+      if( std::abs(acceleration) > 20.0 ) // 20mm/s^2 ?
+      { 
+        if( std::abs(speed - last_speed) > 8 ) 
+        {
+          speed = last_speed + 8 * (short)(acceleration / std::abs(acceleration));
+        }
+      }
+      oss << "[" << speed << "]";
+      std::cout << oss.str() << std::endl;
+
+      //update last_speed
+      last_speed = speed;
+      //update last_timestamp
+      last_timestamp = curr_timestamp;
+      
     }
   }
 
 private:
   bool is_enabled;
   short last_speed;
-  unsigned short last_timestamp;
+//  unsigned short last_timestamp;
+  ecl::TimeStamp last_timestamp;
 };
 
 } // namespace kobuki
