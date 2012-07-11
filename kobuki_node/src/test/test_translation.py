d513b2bccad560df1bae6bea6bdc2842e1fb813d
Kim Min Soo <ms.kim8717@gmail.com>
2012-07-11 15:41:44 +0900

    modified test_translation

--- kobuki_node/src/test/test_translation.py
+++ kobuki_node/src/test/test_translation.py
@@ -32,45 +32,119 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-# Puts the robot into continual rotation - useful for aging/battery tests.
-
 import roslib; roslib.load_manifest('kobuki_node')
 import rospy
 import math
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from kobuki_comms.msg import BumperEvent
+from kobuki_comms.msg import Sound
 
-rospy.init_node("test_rotation")
-pub = rospy.Publisher('/cmd_vel',Twist)
-freq = 5
-rate = rospy.Rate(freq)
-twist = Twist()
-yaw_rate = 0.8
-twist.linear.x = 0
-twist.linear.y = 0
-twist.linear.z = 0
-twist.angular.x = 0
-twist.angular.y = 0
-twist.angular.z = yaw_rate
-rotate_count = 0
-max_rotate_count = freq*int(3.14/yaw_rate)
-start = rospy.get_rostime()
-while not rospy.is_shutdown():
-    if rotate_count == max_rotate_count:
-        if twist.angular.z > 0:
-            mod = -1.0
-        else:
-            mod = 1.0
-        update = mod*yaw_rate/10.0
-        while math.fabs(twist.angular.z) <= yaw_rate:
-            twist.angular.z = twist.angular.z + update
-            pub.publish(twist)
-            rospy.sleep(0.04)
-        # Make sure it is exact so the inequality in the while loop doesn't mess up next time around
-        twist.angular.z = mod*yaw_rate
-        rotate_count = 0
-    else:
-        rotate_count += 1
-    now = rospy.get_rostime()
-    rospy.loginfo("Rotate: %ds", now.secs - start.secs)
-    pub.publish(twist)
-    rate.sleep()
+class Test_Translation:
+	# constructor
+	def __init__(self):
+		rospy.init_node("test_translation")
+		rospy.loginfo('hello kobuki')
+		self._init_params()	# initialize params
+		self._init_pubsub()	# initialize pub & sub 	
+	
+	# initialize params
+	def _init_params(self):
+		
+		self.twist = Twist()
+
+		self.last_odom = 0
+		self.odom = 0
+		self.odom_count = 0	# Odometry Information
+		self.distance = 1	# reference distance
+
+		self.bumper = 0	# bumper 
+		self.state = 0 	# bumper state
+
+		self.sound = 0
+
+		self.freq = 5
+		self.rate = rospy.Rate(self.freq)
+
+		self.twist.linear.x = 0
+		self.twist.linear.y = 0
+		self.twist.linear.z = 0
+		self.twist.angular.x = 0
+		self.twist.angular.y = 0
+		self.twist.angular.z = 0
+
+	def _init_pubsub(self):
+		self.cmd_vel_pub = rospy.Publisher("/cmd_vel", Twist)
+		self.odom_sub = rospy.Subscriber("/odom", Odometry, self.OdomInfoCallback)
+		self.bumper_event_sub = rospy.Subscriber("/mobile_base/events/bumper", BumperEvent, self.BumperEventCallback)
+		self.sound_sub = rospy.Subscriber("/mobile_base/commands/sound", Sound, self.SoundInfoCallback)
+
+
+	def OdomInfoCallback(self,data):
+		self.odom = data.pose.pose.position.x
+
+		if(self.odom_count == 0):
+			self.last_odom = self.odom
+			self.odom_count = self.odom_count + 1
+		
+	def BumperEventCallback(self,data):
+		self.state = data.state
+		self.bumper = data.bumper
+
+	def SoundInfoCallback(self,data):
+		self.sound = data.value
+
+
+	def ShowOdomInfo(self):
+
+		if math.fabs((self.last_odom+1)-self.odom) <= 0.01:
+			self.twist.linear.x = 0.02
+			self.cmd_vel_pub.publish(self.twist)
+
+		elif self.odom >= self.last_odom+1 :
+			self.twist.linear.x = 0
+			self.cmd_vel_pub.publish(self.twist)
+		else:
+			self.twist.linear.x = 0.2
+			self.cmd_vel_pub.publish(self.twist)
+
+		if self.state == 1:
+			while not self.state == 0:
+				rospy.loginfo("bumper event")
+				self.twist.linear.x = 0
+				self.cmd_vel_pub.publish(self.twist)
+				print('---- bumper event ----')
+			self.state = 0
+
+		if self.odom > 0:
+			rospy.loginfo("info_odom : %.4f start_odom : %.4f dist_percent %.4f cmd_vel : %.4f ", self.odom, self.last_odom, (self.odom -self.last_odom)*100, self.twist.linear.x)
+
+	def ShowBumperEventInfo(self):
+		if(self.state == BumperEvent.RELEASED):
+			state = "released"
+		else:
+			state = "pressed"
+		if(self.bumper == BumperEvent.LEFT):
+			bumper = "Left"
+		elif(self.bumper == BumperEvent.CENTER):
+			bumper = "Center"
+		else:
+			bumper = "Right"
+		rospy.loginfo("%s bumper is %s", bumper, state)
+
+
+def test_trans_main():
+	test_trans_obj = Test_Translation()
+
+	while not rospy.is_shutdown():
+		test_trans_obj.ShowOdomInfo() # odom
+	
+#		test_trans_obj.ShowBumperEventInfo()
+
+#		test_trans.obj.ShowSoundInfo()
+
+	rospy.spin()
+
+
+if __name__ == '__main__':
+	test_trans_main()
