60192f036ce633ac303d04ddf5f6a62ae704459a
Younghun Ju <yhju83@gmail.com>
2012-09-14 21:29:52 +0900

    Updated test_sound.py to wait until at least one subscriber is connected.

--- kobuki_testsuite/scripts/test_sounds.py
+++ kobuki_testsuite/scripts/test_sounds.py
@@ -43,6 +43,16 @@ texts = ["On", "Off", "Recharge", "Button", "Error", "CleaningStart", "CleaningE
 rospy.init_node("test_sounds")
 pub = rospy.Publisher('/mobile_base/commands/sound', Sound)
 rate = rospy.Rate(0.5)
+
+# Added below two line of code
+# to wait until at least one subscriber is present or connected.
+# Because rospy.Publisher will skip(or eat) first certain messages, if you publish just after rospy.init()
+# Without this patch, first message - Sound.ON will never be published.
+# I think this is a bug of rospy
+# Younghun Ju
+while not pub.get_num_connections():
+  rate.sleep()
+
 msg = Sound()
 while not rospy.is_shutdown():
     for sound, text in zip(sounds, texts):
