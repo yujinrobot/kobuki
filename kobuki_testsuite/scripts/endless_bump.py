ea42fd59e06c07482a9b18d0c267dd37b4c593ba
Younghun Ju <yhju83@gmail.com>
2012-10-18 01:03:08 +0900

    kobuki_testsuite : Updated endless_bump.py to actually works.

--- kobuki_testsuite/scripts/endless_bump.py
+++ kobuki_testsuite/scripts/endless_bump.py
@@ -37,29 +37,84 @@
 import roslib; roslib.load_manifest('kobuki_testsuite')
 import rospy
 
+from tf.transformations import euler_from_quaternion
+from math import degrees, radians
+
 import random
 
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 from kobuki_comms.msg import BumperEvent
 
-twist = Twist()
+
+wz=0.0
+count=0
+state = 'translation'
+theta = 0.0
+theta_goal = 0.0
+
+def wrapToPi(x):
+  import numpy as np
+  return np.mod(x+np.pi,2*np.pi)-np.pi
+  
+def OdometryCallback(data):
+  global theta
+  quat = data.pose.pose.orientation
+  q = [quat.x, quat.y, quat.z, quat.w]
+  roll, pitch, yaw = euler_from_quaternion(q)
+  theta = yaw
 
 def BumperEventCallback(data):
   #data.bumper
-  twist.angular.z = 3.141592*random.uniform(-1,1)
+  global state
+  global count
+  state = 'step_back'
+  count = 0
   
+def transition():
+  global state
+  global count
+  global theta
+  global theta_goal
+  global wz
+
+  twist = Twist()
+  if state == 'translation':
+    twist.linear.x = 0.3
+
+  elif state == 'rotation':
+    if abs(wrapToPi(theta_goal - theta)) < radians(1.0) :
+      state = 'translation'
+    else:
+      twist.angular.z = wz
+
+  elif state == 'step_back':
+    if count > 10:
+      rand = 3.141592*random.uniform(-1,1)
+      theta_goal = wrapToPi( theta + rand ) 
+      wz = 1.5 * rand/abs(rand)
+      state = 'rotation'
+    else:
+      count += 1
+      twist.linear.x = -0.1
+      
+  return twist
+
 if __name__ == '__main__':
   rospy.init_node('endless_bump')
-  rate = rospy.Rate(10)
+  rate = rospy.Rate(50)
 
   rospy.Subscriber("/mobile_base/events/bumper", BumperEvent, BumperEventCallback)
+  rospy.Subscriber("/odom", Odometry, OdometryCallback)
   pub = rospy.Publisher("cmd_vel", Twist)
+  # state
   # subscribe bump
+  # 
   # bump callback:
-  #   turn robot randomly
+  #   turn robot randomly; 30deg < x < 180deg or -180deg < x < -30deg
   #   pub cmd_vel 
-  twist.angular.z = 3.141592*random.uniform(-1,1)
   while not rospy.is_shutdown():
+    twist = transition()
     pub.publish(twist)
     rate.sleep()
  
