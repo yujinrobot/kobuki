39e2d9e74d190cf882be69919d4baff184fe9b0d
Daniel Stonier <d.stonier@gmail.com>
2012-11-01 16:05:08 +0900

    split widget and plugin interfaces for climbing module.

--- kobuki_qtestsuite/src/kobuki_qtestsuite/climbing.py
+++ kobuki_qtestsuite/src/kobuki_qtestsuite/climbing.py
@@ -1,4 +1,5 @@
 import os
+from QtCore import Signal, Slot
 import roslib
 roslib.load_manifest('kobuki_qtestsuite')
 import rospy
@@ -6,10 +7,14 @@ import rospy
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
 from python_qt_binding.QtGui import QWidget
+from rqt_py_common.extended_combo_box import ExtendedComboBox
+
+from geometry_msgs.msg import Twist
 
 # Local resource imports
 import resources.common
 import resources.climbing
+from .climbing_widget import ClimbingWidget
 
 class Climbing(Plugin):
 
@@ -19,20 +24,15 @@ class Climbing(Plugin):
         self.setObjectName('Climbing')
 
         # create QWidget
-        self._widget = QWidget()
-        # get path to UI file which is a sibling of this file
-        # in this example the .ui file is in the same folder as this Python file
-        ui_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ui', 'climbing.ui')
-        # extend the widget with all attributes and children from UI file
-        loadUi(ui_file, self._widget)
+        self._widget = ClimbingWidget()
+
         # give QObjects reasonable names
         self._widget.setObjectName('ClimbingUi')
         # add widget to the user interface
         context.add_widget(self._widget)
 
     def shutdown_plugin(self):
-        # TODO unregister all publishers here
-        pass
+        self._widget.shutdown()
 
     def save_settings(self, plugin_settings, instance_settings):
         # TODO save intrinsic configuration, usually using:
@@ -43,6 +43,10 @@ class Climbing(Plugin):
         # TODO restore intrinsic configuration, usually using:
         # v = instance_settings.value(k)
         pass
+
+    ##########################################################################
+    # Slot Callbacks
+    ##########################################################################
 
     #def trigger_configuration(self):
         # Comment in to signal that the plugin has a way to configure it
