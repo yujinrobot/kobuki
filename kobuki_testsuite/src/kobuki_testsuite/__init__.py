24120382e749545479731490c876b8fb470b6598
Daniel Stonier <d.stonier@gmail.com>
2012-11-06 09:40:06 +0900

    travel forward motion finished.

--- kobuki_testsuite/src/kobuki_testsuite/__init__.py
+++ kobuki_testsuite/src/kobuki_testsuite/__init__.py
@@ -2,4 +2,3 @@
 from .motion_wander import SafeWandering
 from .motion_travel_forward import TravelForward
 from .rotate import RotateTest 
-from .foo import bar
--- kobuki_testsuite/src/kobuki_testsuite/motion_travel_forward.py
+++ kobuki_testsuite/src/kobuki_testsuite/motion_travel_forward.py
@@ -1,8 +1,11 @@
-import threading
+import math
 import roslib; roslib.load_manifest('kobuki_testsuite')
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Pose
 from nav_msgs.msg import Odometry
+
+# Local imports
+import utils
     
 '''
   Travels forward a set distance. 
@@ -12,8 +15,10 @@ class TravelForward(object):
       Initialise everything
 
       API:
-        start(xxx,xxx,xxx) - start travelling. 
+        init(speed,distance) : (re)initialise parameters 
         stop()  - stop.
+        execute() - pass this to a thread to run
+        shutdown() - cleanup
       
       @param topic names
       @type strings
@@ -23,61 +28,55 @@ class TravelForward(object):
         self.cmd_vel_publisher = rospy.Publisher(cmd_vel_topic, Twist)
         self._speed = 0.7
         self._distance = 1.0
+        self._current_pose = Pose()
+        self._starting_pose = Pose()
         self._stop = False
-        self._current_pose = None
-        self._starting_pose = None
-        self._run_thread = None
+        self._running = False
     
+    def init(self, speed, distance):
+        self._speed = speed
+        self._distance = distance
+        
     def shutdown(self):
         self.stop()
         self.cmd_vel_publisher.unregister()
         self.odom_subscriber.unregister()
-
-    def start(self, speed, distance, run_finished_callback):
-        '''
-          Set the speed and the travel distance, then launch the thread.
-          
-          Ignore the command if the thread is running.
-        '''
-        self._speed = speed
-        self._distance = distance
-        if self.is_alive():
-            rospy.logerr("Kobuki TestSuite: already executing a motion, ignoring the request")
-            return False
-        else:
-            self._starting_pose = self._current_pose
-            self._run_thread = WorkerThread(self._run, run_finished_callback)
-            self._run_thread.start()
-            return True
-
+    
     def stop(self):
         self._stop = True
-        if self._run_thread:
-            self._run_thread.join()
-            self._run_thread = None
-        cmd = Twist()
-        cmd.linear.x = 0.0
-        self.cmd_vel_publisher.publish(cmd)
-        
-    def _run(self):
+
+    def execute(self):
+        '''
+          Drop this into threading.Thread or QThread for execution
+        '''
+        if self._running:
+            rospy.logerr("Kobuki TestSuite: already executing a motion, ignoring the request")
+            return
         self._stop = False
+        self._running = True
         rate = rospy.Rate(10)
         self._current_speed = 0.0
         current_distance_sq = 0.0
+        distance_sq = self._distance*self._distance
         while not self._stop and not rospy.is_shutdown():
-            if current_distance_sq < distance_sq:
-                self.stop()
+            if current_distance_sq > distance_sq:
+                break
             else:
                 current_distance_sq = (self._current_pose.position.x - self._starting_pose.position.x)*(self._current_pose.position.x - self._starting_pose.position.x) + \
                                    (self._current_pose.position.y - self._starting_pose.position.y)*(self._current_pose.position.y - self._starting_pose.position.y)
-                #current_distance += 0.01
+                #current_distance_sq += 0.01 # uncomment this and comment above for debugging
                 print("Distance %s"%math.sqrt(current_distance_sq))
-                if self._current_speed < speed:
+                if self._current_speed < self._speed:
                     self._current_speed += 0.01
                 cmd = Twist()
                 cmd.linear.x = self._current_speed
                 self.cmd_vel_publisher.publish(cmd)
             rate.sleep()
+        if not rospy.is_shutdown():
+            cmd = Twist()
+            cmd.linear.x = 0.0
+            self.cmd_vel_publisher.publish(cmd)
+        self._running = False
         
     ##########################################################################
     # Ros Callbacks
