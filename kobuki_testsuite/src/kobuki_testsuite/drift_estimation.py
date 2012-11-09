5d6ebea828ba76164858af7c060d4a1e4db4c6f8
Daniel Stonier <d.stonier@gmail.com>
2012-11-09 20:17:15 +0900

    started drift estimation test.

---
+++ kobuki_testsuite/src/kobuki_testsuite/drift_estimation.py
@@ -0,0 +1,60 @@
+from math import *
+import roslib; roslib.load_manifest('kobuki_qtestsuite')
+import yaml
+import rospy
+from sensor_msgs.msg import LaserScan
+
+class ScanToAngle:
+    def __init__(self, scan_topic):
+        self.min_angle = -0.3
+        self.max_angle = 0.3
+        #self.pub = rospy.Publisher('scan_angle', ScanAngle)
+        self.scan_subscriber = rospy.Subscriber(scan_topic, LaserScan, self.scan_callback)
+
+    def init(self, min_angle, max_angle):
+        self.min_angle = -0.3
+        self.max_angle = 0.3
+        
+    def shutdown(self):
+        self.stop()
+        while self._running:
+            self.rate.sleep()
+        self.scan_angle_publisher.unregister()
+        self.scan_subscriber.unregister()
+
+    def scan_callback(self, msg):
+        angle = msg.angle_min
+        d_angle = msg.angle_increment
+        sum_x = 0
+        sum_y = 0
+        sum_xx = 0
+        sum_xy = 0
+        num = 0
+        for r in msg.ranges:
+            if angle > self.min_angle and angle < self.max_angle and r < msg.range_max:
+                x = sin(angle) * r
+                y = cos(angle) * r
+                sum_x += x
+                sum_y += y
+                sum_xx += x*x
+                sum_xy += x*y
+                num += 1
+            angle += d_angle
+        if num > 0:
+            angle=atan2((-sum_x*sum_y+num*sum_xy)/(num*sum_xx-sum_x*sum_x), 1)
+            print("Scan Angle: %s"%str(angle))
+            #res = ScanAngle()
+            #res.header = msg.header
+            #res.scan_angle = angle
+            self.pub.publish(res)
+        else:
+            rospy.logerr("Please point me at a wall.")
+
+def main():
+    rospy.init_node('scan_to_angle')
+    s = ScanToAngle('/scan')
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
+
