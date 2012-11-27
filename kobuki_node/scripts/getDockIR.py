3cbf1eb649003a4b64ac8bc78b65636598f232ed
Younghun Ju <yhju83@gmail.com>
2012-11-27 23:45:37 +0900

    Added an useful script related to automatic docking. It converts raw sensor reading of ir sensor to human friendly format.

---
+++ kobuki_node/scripts/getDockIR.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#AUTHOR: Younghun Ju <yhju@yujinrobot.comm>, <yhju83@gmail.com>
+
+import roslib; roslib.load_manifest('kobuki_node')
+import rospy
+
+from kobuki_msgs.msg import DockInfraRed
+
+class Converter(object):
+
+  def __init__(self):
+    rospy.init_node("dock_ir_intepreter", anonymous=True)
+    self.sub  = rospy.Subscriber("dock_ir", DockInfraRed, self.DockIRCallback)
+    self.stack = []
+
+
+  def DockIRCallback(self,data):
+    #v1 not filtering
+    array = [ ord(x) for x in data.data]
+    #v2 filtering
+    self.stack.append(array)
+    while len(self.stack) > 10:
+      del self.stack[0]
+    print len(self.stack)
+
+    array = [0,0,0]
+    for i in range(len(self.stack)):
+      for j in range(3):
+        array[j] |= self.stack[i][j]
+
+
+    ostr = ""
+    ostr_top = "[far ] "; ostr_bot = "[near] "; ostr_bin="[bin ] "; ostr_dec="[dec ] "
+    for ir in array:
+      #ver1: just digits: 
+      #ostr += "{0:3d}".format(ord(array[i])) + " "
+      #ver2: just binary number: 
+      #ostr += "{0:#08b}".format(ord(array[i]))[2:] + " "
+      #ver3: [far]/[near] = [R|C|L]/[R|C|L] for each channel
+      #ostr_top = ""; ostr_bot = ""
+      top = ir>>3; bot =ir
+      ostr_top += " R" if top&1 else " ~" 
+      ostr_top += "|C" if top&2 else "|~" 
+      ostr_top += "|L" if top&4 else "|~" 
+      ostr_bot += " R" if bot&1 else " ~" 
+      ostr_bot += "|C" if bot&2 else "|~" 
+      ostr_bot += "|L" if bot&4 else "|~"
+      ostr_top += " "; ostr_bot += " "
+      ostr_bin += "{0:#08b}".format(ir)[2:] + " "
+      #ostr_dec += "{0:3d}".format(ir) + " "
+    ostr = ostr_top + "\n\r" + ostr_bot + "\n" + ostr_bin# + "\n" + ostr_dec
+      #verN: average
+      #verN: 
+    print ostr
+
+if __name__ == '__main__':
+  try:
+    instance = Converter()
+    print 
+    print "It converts dock_ir data to human friendly format."
+    print
+    rospy.spin()
+  except rospy.ROSInterruptException: pass
