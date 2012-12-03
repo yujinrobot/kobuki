a8f19525d9dcad30d03a76a4c6677048f6fdeab5
Younghun Ju <yhju83@gmail.com>
2012-12-03 09:51:20 +0900

    kobuki_node : Updated getDockIR.py script to print bumper and charger state also.

--- kobuki_node/scripts/getDockIR.py
+++ kobuki_node/scripts/getDockIR.py
@@ -5,23 +5,36 @@ import roslib; roslib.load_manifest('kobuki_node')
 import rospy
 
 from kobuki_msgs.msg import DockInfraRed
+from kobuki_msgs.msg import SensorState
 
 class Converter(object):
 
   def __init__(self):
     rospy.init_node("dock_ir_intepreter", anonymous=True)
-    self.sub  = rospy.Subscriber("dock_ir", DockInfraRed, self.DockIRCallback)
+    self.sub_dock_ir = rospy.Subscriber("/mobile_base/sensors/dock_ir", DockInfraRed, self.DockIRCallback)
+    self.sub_core    = rospy.Subscriber("/mobile_base/sensors/core" , SensorState, self.SensorStateCallback)
     self.stack = []
+    self.bumper = 0
+    self.charger = 0
 
+
+  def SensorStateCallback(self,data):
+    self.bumper = data.bumper
+    self.charger = data.charger
 
   def DockIRCallback(self,data):
     #v1 not filtering
     array = [ ord(x) for x in data.data]
+    array.reverse()
     #v2 filtering
     self.stack.append(array)
     while len(self.stack) > 10:
       del self.stack[0]
-    print len(self.stack)
+    print 
+    print "window: ", len(self.stack), "charger: ", self.charger, "[ON]" if self.charger else "[  ]", "bumper: ["\
+      , "L" if self.bumper&4 else "~"\
+      , "C" if self.bumper&2 else "~"\
+      , "R" if self.bumper&1 else "~", "]"
 
     array = [0,0,0]
     for i in range(len(self.stack)):
