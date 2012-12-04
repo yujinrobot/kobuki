ee54536fcd4663483b686a11498e24539a02e3f7
Younghun Ju <yhju83@gmail.com>
2012-12-05 03:00:01 +0900

    Added DockDriveControl.py, an integrated control panel of docking module to provides fast response and control. It will replace many scripts which too simple and slow.

--- kobuki_node/scripts/cancel_dock.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rostopic pub /mobile_base/commands/cancel_dock std_msgs/String 'disable' -1
--- kobuki_node/scripts/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rostopic pub /mobile_base/commands/do_dock std_msgs/String 'run' -1
