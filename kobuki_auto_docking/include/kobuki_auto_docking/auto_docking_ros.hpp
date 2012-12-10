24fc406fbe78120dfbe49f8c3caa7f11263f2b12
Younghun Ju <yhju83@gmail.com>
2012-12-10 15:25:35 +0900

    kobuki_auto_docking : Added several skeletal subscribers in library, including synchronizer of message_filters package.

--- kobuki_auto_docking/include/kobuki_auto_docking/auto_docking_ros.hpp
+++ kobuki_auto_docking/include/kobuki_auto_docking/auto_docking_ros.hpp
@@ -19,15 +19,21 @@
 *****************************************************************************/
 
 #include <ros/ros.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <std_msgs/String.h>
 #include <nav_msgs/Odometry.h>
 #include <kobuki_msgs/SensorState.h>
 #include <kobuki_msgs/DockInfraRed.h>
-
 #include <ecl/geometry/pose2d.hpp>
 #include "auto_docking.hpp"
 
 namespace kobuki 
 {
+
+typedef message_filters::sync_policies::ApproximateTime<kobuki_msgs::SensorState, kobuki_msgs::DockInfraRed> SyncPolicy;
 
 class AutoDockingROS
 {
@@ -44,15 +50,28 @@ public:
 
 private:
   bool shutdown_requested;
+  ros::NodeHandle nh_;
   AutoDocking dock_;
-  ros::Subscriber odom_, dock_ir_, core_sensors_;
-  ros::Publisher velocity_commander_;
+  ros::Subscriber odom_, dock_ir_, core_sensors_, do_dock_, cancel_dock_, debug_;
+  ros::Publisher velocity_commander_, motor_power_enabler_;
+
+//  message_filters::Subscriber<kobuki_msgs::DockInfraRed> ir_sub_;
+//  message_filters::Subscriber<kobuki_msgs::SensorState> core_sub_;
+  boost::shared_ptr<message_filters::Subscriber<kobuki_msgs::DockInfraRed> > ir_sub_;
+  boost::shared_ptr<message_filters::Subscriber<kobuki_msgs::SensorState> > core_sub_;
+  boost::shared_ptr<message_filters::Synchronizer<SyncPolicy> > sync_;
+  //boost::shared_ptr<message_filters::TimeSynchronizer<kobuki_msgs::SensorState, kobuki_msgs::DockInfraRed> > sync_;
 
   //ecl::Pose2D odom_;
   void odomCb(const nav_msgs::OdometryPtr msg);
   void coreCb(const kobuki_msgs::SensorStatePtr msg);
   void irCb(const kobuki_msgs::DockInfraRedPtr msg);
 
+  void doCb(const std_msgs::StringPtr msg);
+  void cancelCb(const std_msgs::StringPtr msg);
+  void debugCb(const std_msgs::StringPtr msg);
+
+  void syncCb(const kobuki_msgs::SensorStateConstPtr& core, const kobuki_msgs::DockInfraRedConstPtr& ir);
 };
 
 } //namespace kobuki
--- kobuki_auto_docking/src/auto_docking_ros.cpp
+++ kobuki_auto_docking/src/auto_docking_ros.cpp
@@ -27,9 +27,19 @@ AutoDockingROS::~AutoDockingROS()
 
 bool AutoDockingROS::init(ros::NodeHandle& nh)
 {
+  nh_ = nh;
+  odom_ = nh.subscribe("/odom", 10, &AutoDockingROS::odomCb, this);
   core_sensors_ = nh.subscribe("/mobile_base/sensors/core", 10, &AutoDockingROS::coreCb, this);
   dock_ir_ = nh.subscribe("/mobile_base/sensors/dock_ir", 10, &AutoDockingROS::irCb, this);
-  odom_ = nh.subscribe("/odom", 10, &AutoDockingROS::odomCb, this);
+
+  //core_sub_.subscribe(nh_, "/mobile_base/sensors/core", 10);
+  //ir_sub_.subscribe(nh_, "/mobile_base/sensors/dock_ir", 10);
+  //sync_.reset(new message_filters::Synchronizer<SyncPolicy>(SyncPolicy(10), core_sub_, ir_sub_));
+  core_sub_.reset(new message_filters::Subscriber<kobuki_msgs::SensorState>(nh_, "/mobile_base/sensors/core", 10));
+  ir_sub_.reset(new message_filters::Subscriber<kobuki_msgs::DockInfraRed>(nh_, "/mobile_base/sensors/dock_ir", 10));
+  sync_.reset(new message_filters::Synchronizer<SyncPolicy>(SyncPolicy(10), *core_sub_, *ir_sub_));
+  sync_->registerCallback(boost::bind(&AutoDockingROS::syncCb, *this, _1, _2));
+
   //odom_.subscribe("some_topic");
   //ir_.subscribe("some_topic", AutoDockingROS::ir_cb);
   //velocity_commander_.publish("some_topic");
@@ -41,13 +51,50 @@ bool AutoDockingROS::init(ros::NodeHandle& nh)
  */
 void AutoDockingROS::spin()
 {
+  return;
+
+  message_filters::Subscriber<kobuki_msgs::SensorState> core_sub(nh_, "/mobile_base/sensors/core", 10);
+  message_filters::Subscriber<kobuki_msgs::DockInfraRed> ir_sub(nh_, "/mobile_base/sensors/dock_ir", 10);
+
+#if 1
+  //approx
+  message_filters::Synchronizer<SyncPolicy> sync(SyncPolicy(10), core_sub, ir_sub);
+  sync.registerCallback(boost::bind(&AutoDockingROS::syncCb, *this, _1, _2));
+#endif
+
+#if 0
+  message_filters::TimeSynchronizer<kobuki_msgs::SensorState, kobuki_msgs::DockInfraRed> sync(core_sub, ir_sub, 10);
+  sync.registerCallback(boost::bind(&AutoDockingROS::syncCb, *this, _1, _2));
+#endif
+
+#if 0
+  sync_.reset(new message_filters::Synchronizer<SyncPolicy>(SyncPolicy(10), core_sub, ir_sub));
+  sync_->registerCallback(boost::bind(&AutoDockingROS::syncCb, *this, _1, _2));
+#endif
+
+#if 0
+  sync_.reset(new message_filters::TimeSynchronizer<kobuki_msgs::SensorState, kobuki_msgs::DockInfraRed>(core_sub, ir_sub, 10));
+  sync_->registerCallback(boost::bind(&AutoDockingROS::syncCb, *this, _1, _2));
+#endif
+
+  while(!shutdown_requested);
+
   //msg.data = dock_.target_direction_;
   //velocity_commander_.publish(msg);
-  dock_.auto_dock();
+  //dock_.auto_dock();
 }
 
+void AutoDockingROS::syncCb(const kobuki_msgs::SensorStateConstPtr& core, const kobuki_msgs::DockInfraRedConstPtr& ir)
+{
+  static int n = 0;
+  ROS_INFO_STREAM("syncCb: " << __func__ << "(" << n++ << ")");
+  std::cout << "---1---" << std::endl << core << std::endl;
+  std::cout << "---2---" << std::endl << ir << std::endl;
+}
 void AutoDockingROS::odomCb(const nav_msgs::OdometryPtr msg)
 {
+  static int n = 0;
+  ROS_INFO_STREAM("odomCb: " << __func__ << "(" << n++ << ")");
   //odom_ = data; 
   ;
   //dock_.update(odom_);
@@ -55,11 +102,15 @@ void AutoDockingROS::odomCb(const nav_msgs::OdometryPtr msg)
 
 void AutoDockingROS::coreCb(const kobuki_msgs::SensorStatePtr msg)
 {
+  static int n = 0;
+  ROS_INFO_STREAM("coreCb: " << __func__ << "(" << n++ << ")");
   ;
 }
 
 void AutoDockingROS::irCb(const kobuki_msgs::DockInfraRedPtr msg)
 {
+  static int n = 0;
+  ROS_INFO_STREAM("irCb: " << __func__ << "(" << n++ << ")");
   ;
 }
 
