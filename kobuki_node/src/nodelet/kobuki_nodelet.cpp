3478d9a8795ace7b097cf70cfa3b6dbee05f0a75
v4hn <me@v4hn.de>
2016-06-15 23:59:38 +0200

    make robot_description remappable
    There is no reason to address "/robot_description" instead of
    "robot_description" with a non-private node-handle.
    It breaks namespacing to use '/' in the beginning, and we had
    trouble moving the whole turtlebot_bringup/minimal.launch into
    a namespace because of that.

--- kobuki_node/src/nodelet/kobuki_nodelet.cpp
+++ kobuki_node/src/nodelet/kobuki_nodelet.cpp
@@ -61,7 +61,7 @@ public:
     std::string nodelet_name = this->getName();
     kobuki_.reset(new KobukiRos(nodelet_name));
     // if there are latency issues with callbacks, we might want to move to process callbacks in multiple threads (use MTPrivateNodeHandle)
-    if (kobuki_->init(this->getPrivateNodeHandle()))
+    if (kobuki_->init(this->getPrivateNodeHandle(), this->getNodeHandle()))
     {
       update_thread_.start(&KobukiNodelet::update, *this);
       NODELET_INFO_STREAM("Kobuki : initialised.");
